bd071db9ffc76d8e5db56616e4b3f10b73b58c44
Synkevych Roman <sunkevu4@gmail.com>
2019-08-12 20:33:46 +0300

    Updates

---
+++ precache-manifest.e08336f09bc64c1967fdca2a6657cc52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43309bc2da26957d39c0578d53e6b537",
+    "url": "/react_async_apis/index.html"
+  },
+  {
+    "revision": "e9335f50c7c500e097d1",
+    "url": "/react_async_apis/static/css/main.6b92a3f7.chunk.css"
+  },
+  {
+    "revision": "0f96490e3eb2cb3e2448",
+    "url": "/react_async_apis/static/js/2.ec057f4a.chunk.js"
+  },
+  {
+    "revision": "e9335f50c7c500e097d1",
+    "url": "/react_async_apis/static/js/main.82af7ecb.chunk.js"
+  },
+  {
+    "revision": "7b5276b19660979dab3d",
+    "url": "/react_async_apis/static/js/runtime~main.ed0304fd.js"
+  }
+]);
